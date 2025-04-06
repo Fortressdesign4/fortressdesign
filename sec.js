81312c29fdae52ca149338da45762444d5836b22
Fortressdesign4 <fortressdesign4@gmail.com>
2025-04-06 18:56:15 +0000

    Update sec.js

--- sec.js
+++ sec.js
@@ -17,7 +17,7 @@ export function cleanFortress(e) {
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toUTCString();
         }
-        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+        document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=None; Secure";
     };
 
     // Show cookie consent banner
